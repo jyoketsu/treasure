d3254a463ea939d9bcc7a3946b801175d09851be
xujie <xujie@qingtime.com>
2019-09-12 10:03:56 +0800

    2019-09-12 10:03:56

--- src/components/portal/Portal.js
+++ src/components/portal/Portal.js
@@ -94,48 +94,48 @@ class Plugins extends Component {
         return (
             <div className="portal-plugin" style={style}>
                 {
-                    plugins.map((plugin, index) => (
-                        <div
-                            key={index}
-                            className="station-plugin"
-                            onClick={
-                                () => {
-                                    switch (plugin.publish) {
-                                        case 1: window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
-                                            break;
-                                        case 2: content.role > 2 ?
-                                            message.info('没有权限访问！') :
-                                            window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
-                                            break;
-                                        case 3:
-                                            if (content.role || plugin.isSeePlugin) {
-                                                window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
-                                            } else {
-                                                if (user.isGuest) {
-                                                    message.info('请登录');
-                                                    return;
-                                                } else {
-                                                    this.rightAnswer = plugin.answer;
-                                                    this.question = {
-                                                        type: 'plugin',
-                                                        param: {
-                                                            url: `${plugin.url}/${content.domain}?token=${token}`,
-                                                            key: plugin._key,
-                                                        },
-                                                    }
-                                                    this.switchPluginVisible(plugin.question, '');
-                                                }
-                                            }
-                                            break;
-                                        default: break;
-                                    }
-                                }
-                            }
-                        >
-                            <i style={{ backgroundImage: `url(${plugin.icon})` }}></i>
-                            <span>{plugin.name}</span>
-                        </div>
-                    ))
+                    // plugins.map((plugin, index) => (
+                    //     <div
+                    //         key={index}
+                    //         className="station-plugin"
+                    //         onClick={
+                    //             () => {
+                    //                 switch (plugin.publish) {
+                    //                     case 1: window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
+                    //                         break;
+                    //                     case 2: content.role > 2 ?
+                    //                         message.info('没有权限访问！') :
+                    //                         window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
+                    //                         break;
+                    //                     case 3:
+                    //                         if (content.role || plugin.isSeePlugin) {
+                    //                             window.open(`${plugin.url}/${content.domain}?token=${token}`, '_blank');
+                    //                         } else {
+                    //                             if (user.isGuest) {
+                    //                                 message.info('请登录');
+                    //                                 return;
+                    //                             } else {
+                    //                                 this.rightAnswer = plugin.answer;
+                    //                                 this.question = {
+                    //                                     type: 'plugin',
+                    //                                     param: {
+                    //                                         url: `${plugin.url}/${content.domain}?token=${token}`,
+                    //                                         key: plugin._key,
+                    //                                     },
+                    //                                 }
+                    //                                 this.switchPluginVisible(plugin.question, '');
+                    //                             }
+                    //                         }
+                    //                         break;
+                    //                     default: break;
+                    //                 }
+                    //             }
+                    //         }
+                    //     >
+                    //         <i style={{ backgroundImage: `url(${plugin.icon})` }}></i>
+                    //         <span>{plugin.name}</span>
+                    //     </div>
+                    // ))
                 }
             </div>
         );
